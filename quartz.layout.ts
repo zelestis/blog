1fb1b0d6de7ee41a0bb454ca8d4f512fbf035a12
Nick van der Merwe <nick.jvdmerwe@gmail.com>
2024-10-06 13:08:20 +1100

    Quartz sync: Oct 6, 2024, 1:08 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -30,9 +30,9 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    //  Component.Graph(),
+    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
-    // Component.Backlinks(),
+    Component.Backlinks(),
   ],
 }
 
